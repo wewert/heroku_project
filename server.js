f496e4512c3082acc8ccab75e620973bc92df498
Ken Lee <wewert@gmail.com>
2018-02-24 13:36:14 -0700

    adjusted port

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = 3000;
+var port = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -91,6 +93,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
